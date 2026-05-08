6ee4b87e0dbb3eb621174b7acb75f8196b461688
varshiniui <varshini562021@gmail.com>
2026-05-08 11:06:03 +0530

    second Commit

--- src/main/java/cutoffmark/EngineeringCutoff.java
+++ src/main/java/cutoffmark/EngineeringCutoff.java
@@ -1,5 +1,23 @@
 package cutoffmark;
-
+import java.util.Scanner;
 public class EngineeringCutoff {
-
+public int CutoffMark (int Maths,int Physics,int Chemistry) {
+	return (Maths/2)+(Physics/4)+(Chemistry/4);
+}
+public static void main (String [] args) {
+	EngineeringCutoff cm = new EngineeringCutoff();
+	Scanner sc = new Scanner (System.in);
+	
+	System.out.println("Engineering CutOffMark");
+	System.out.println("Enter Physics Mark:");
+	int Physics = sc.nextInt():
+	System.out.println("Enter Maths Mark:");
+	int Maths = sc.nextInt();
+	System.out.println("Enter Chemistry Mark");
+	int Chemistry = sc.nextInt();
+	System.out.print("Cutoff Mark is :");
+	int cutoff = cm.CutoffMark(Maths,Physics,Chemistry);
+	
+	sc.close();
+}
 }
--- src/test/java/cutoffmark/EngineeringCutoffTest.java
+++ src/test/java/cutoffmark/EngineeringCutoffTest.java
@@ -1,5 +1,16 @@
 package cutoffmark;
-
-public class EngineeringCutoffTest {
+import static org.java.jupiter.api.Assertions.*;
+import org.java.jupiter.api.BeforeAll;
+import org.java.jupiter.api.Test;
+class EngineeringCutoffTest {
+	static EngineeringCutoff cm;
+@BeforeAll
+static void setUpBeforeClass () throws Exception{
+	cm = new EngineeringCutoff();
+}
+@Test
+void CutoffMarkTest() {
+	assertEquals(92,cm.CutoffMark(94,85,97));
+}
 
 }
